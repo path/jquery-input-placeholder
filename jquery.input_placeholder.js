953d898ad635c3d545a1bcb64869e495be942410
Dan Dofter <dan@dofter.com>
2010-07-21 14:44:28 -0700

    Refactored plugin to be more idomatic JQuery; fixed issue with placeholder not going away when typing a space

--- jquery.input_placeholder.js
+++ jquery.input_placeholder.js
@@ -1,68 +1,59 @@
 /*
-* Input Placeholder 1.0 - jQuery Plugin
+* Input Placeholder 1.1 - jQuery Plugin
 *
 * Copyright 2010, Graham Blache, Dan Dofter
 * Dual licensed under the MIT or GPL Version 2 licenses.
 */
 (function($){
   
-  $.input_placeholder = function() {
-    $('input[data-placeholder]').each(function(){
+  $.fn.inputPlaceholder = function() {
+    return this.each(function(){
+      $(this).find('input[data-placeholder]').each(function(){
+        var $input = $(this);
 
-      var input = $(this);
+        var $container = $('<div><span>' + $input.attr('data-placeholder') + '</span></div>');
+        $container.css('position','relative');
 
-      // Build container
-      var container = $('<div><span>' + $(input).attr('data-placeholder') + '</span></div>');
-      $(container).css('position','relative');
+        var $span = $container.find('span');
 
-      // Position placeholder
-      var placeholder = $(container).find('span');
-      $(placeholder).css('position','absolute');
-      // The 2 extra pixels account for the cursor
-      var left = parseInt($(input).css('padding-left')) + 2 + parseInt($(input).css('margin-left')) + parseInt($(input).css('border-left-width'));
-      $(placeholder).css('left', left);
-      var top = parseInt($(input).css('padding-top')) + parseInt($(input).css('margin-top')) + parseInt($(input).css('border-top-width'));
-      $(placeholder).css('top', top);
+        $span.css({
+          'position': 'absolute', 
+          'left': $input.css('padding-left'),
+          'top': $input.css('padding-top'),
+          'font-size': $input.css('font-size'),
+          'font-family': $input.css('font-family'),
+          'user-select': 'none',
+          '-moz-user-select': 'none',
+          '-webkit-user-select': 'none'
+        });
 
-      // Style placeholder
-      $(placeholder).css('font-size', $(input).css('font-size'));
-      $(placeholder).css('font-family', $(input).css('font-family'));
-      
-      // Disable selection
-      $(placeholder).css('user-select','none').css('-moz-user-select','none').css('-webkit-user-select','none');    
-      
-      // Hide placeholder if its been prepopulated
-      if ($(input).val().length > 0) {
-        $(placeholder).hide();
-      }
-
-      // Insert the container into the dom
-      $(input).after(container);
-      $(input).appendTo(container);
-      
-      // Add the placeholder class so it can be styled
-      $(placeholder).addClass('placeholder');
-
-      // Focus on input if placeholder is clicked
-      $(placeholder).click(function(){
-        $(input).focus();
-      });
-      
-      // Interval, detect all changes to input
-      var input_value = $(input).val();
-      setInterval(function(){
-        if ($(input).val() != input_value) {
-          if ($.trim($(input).val()).length == 0) {
-            $(this).val('');
-            $(placeholder).show();
-          } else {
-            $(placeholder).hide();
-          }
-          input_value = $(input).val();
+        if ($input.val().length > 0) {
+          $container.find('span').hide();
         }
-      }, 50);
 
+        $input.after($container);
+        $input.appendTo($container);
+
+        $span.addClass('placeholder');
+
+        $span.click(function(){
+          $input.focus();
+        });
+
+        var input_value = $input.val();
+        setInterval(function(){
+          if ($input.val() != input_value) {
+            if ($input.val().length == 0) {
+              $(this).val('');
+              $container.find('span').show();
+            } else {
+              $container.find('span').hide();
+            }
+            input_value = $input.val();
+          }
+        }, 50);
+      });
     });
-  }
+  };
   
 })(jQuery);
